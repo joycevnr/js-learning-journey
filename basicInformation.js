9003ea14a6fe1fe7db44b972e5e84d17c4d986b9
joycevnr <joycevitoria150@gmail.com>
2024-03-17 00:57:46 -0300

    update basicInformation

---
+++ basicInformation.js
@@ -0,0 +1,24 @@
+// JavaScript é uma linguagem case sensitive (isso significa que a linguagem vê diferença entre letras maiúsculas e minúsculas)
+
+// Exemplo de declaração de variáveis
+let variavel; // Declarar variável que pode ser modificada
+const constante = 10; // Declarar constante cujo valor não pode ser modificado
+var outraVariavel; // Declarar variável com var (evitar o uso)
+
+// Exemplo de atribuição e saída de dados
+let numero = 12;
+console.log(numero); // Saída: 12
+
+// Função do operador +
+console.log("5" + "5"); // Saída: "55" (concatenação de strings)
+console.log(5 + 5); // Saída: 10 (adição de números)
+
+// Exemplo de uso do typeof
+let variavelExemplo = 2;
+console.log(typeof variavelExemplo); // Saída: number
+
+// Comentário
+/*.....
+.....*/       //comentário de mais de uma linha
+
+//  comentário de uma linha
