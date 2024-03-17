c22e0ac14de63352d84a1c62dad31cee014119a0
joycevnr <joycevitoria150@gmail.com>
2024-03-17 01:02:04 -0300

    mudança de pasta

---
+++ Contents/operators.js
@@ -0,0 +1,32 @@
+// Operadores aritméticos:
+// Os operadores aritméticos em JavaScript são utilizados para realizar cálculos matemáticos.
+
+// Exemplos de operadores aritméticos:
+let soma = 5 + 3; // Soma
+let subtracao = 10 - 4; // Subtração
+let multiplicacao = 6 * 2; // Multiplicação
+let divisao = 8 / 4; // Divisão
+let restoDivisao = 10 % 3; // Resto da divisão
+let potencia = 2 ** 3; // Potenciação
+
+// Exemplo de precedência dos operadores aritméticos:
+let resultado = 5 + 3 / 2; // O resultado é 6.5 (primeiro a divisão, depois a adição).
+let resultadoComParenteses = (5 + 3) / 2; // O resultado é 4 (primeiro a adição, depois a divisão).
+
+// Operadores de atribuição:
+// Os operadores de atribuição são utilizados para atribuir valores a variáveis.
+let x = 10;
+x += 5; // Equivalente a x = x + 5
+
+let y = 20;
+y -= 3; // Equivalente a y = y - 3
+
+// Estruturas de controle:
+// As estruturas de controle são utilizadas para controlar o fluxo de execução do programa.
+// Exemplo de estrutura de controle if-else:
+let idade = 18;
+if (idade >= 18) {
+    console.log("Maior de idade");
+} else {
+    console.log("Menor de idade");
+}
